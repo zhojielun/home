0346123a0458c4c2a50c404c91a8b156dba6f8b7
zhojielun <137139821+zhojielun@users.noreply.github.com>
2024-09-02 13:16:36 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -9,7 +9,7 @@ export const mainStore = defineStore("main", {
       siteStartShow: false, // 建站日期显示
       musicClick: false, // 音乐链接是否跳转
       musicIsOk: false, // 音乐是否加载完成
-      musicVolume: 0, // 音乐音量;
+      musicVolume: 50, // 音乐音量;
       musicOpenState: false, // 音乐面板开启状态
       backgroundShow: false, // 壁纸展示状态
       boxOpenState: false, // 盒子开启状态
